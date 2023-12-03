d5d1ee9e36e93c04530e3be6816bccc24963bd01
PsychoPunkSage <abhinav.prakash319@gmail.com>
2023-12-03 20:39:56 +0530

    feat: completed async+await

--- json_async_await/src/main.rs
+++ json_async_await/src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+use std::{fs::File, io::Write};
+use error_chain::error_chain;
+
+error_chain! {
+    foreign_links {
+        Io(std::io::Error);
+        HttpRequest(reqwest::Error);
+    }
+}
+
+fn create_json_file(path: &str, content: String) -> Result<()> {
+    let mut file = File::create(&path)?;
+    file.write_all(content.as_bytes())?;
+    Ok(())
+}
+
+#[tokio::main]
+async fn submain() -> Result<()> {
+    let args: Vec<_> = std::env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: {} <FileLocation>", args[0]);
+        return Ok(());
+    }
+
+    let res = reqwest::get("http://httpbin.org/get").await?;
+    println!("Status: {:?}", res.status());
+    println!("Header: \n{:?}", res.headers());
+
+    let body = res.text().await?;
+    println!("Body: {:?}", body);
+
+    match create_json_file("test.json", body) {
+        Ok(_) => print!("File Created"),
+        Err(e) => println!("Error: {:?}", e),
+    };
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    submain()?;
+    Ok(())
 }
