5fa7f95715d89a3c0ac74e5b1e53d8294b8b2868
PsychoPunkSage <abhinav.prakash319@gmail.com>
2023-12-03 18:11:48 +0530

    feat: completed json_get_request + able to create JSON res

--- json_get_request/src/main.rs
+++ json_get_request/src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+use error_chain::error_chain;
+use std::io::prelude::*;
+use std::fs::File;
+
+// Define Custom Error
+error_chain! {
+    foreign_links{
+        Io(std::io::Error);
+        HttpRequest(reqwest::Error);
+    }
+}
+
+fn create_json_file(path: &str, content: &str) -> Result<()> {
+    let mut file = File::create(path)?;
+    file.write_all(content.as_bytes())?;
+    Ok(())
+}
+
+
+fn main() -> Result<()> {
+    let args: Vec<_> = std::env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: {} <fileLocation>", args[0]);
+    };
+
+    let mut res = reqwest::blocking::get("http://httpbin.org/get")?;
+    println!("Response: \n{:?}", res);
+    
+    let mut body = String::new();
+    res.read_to_string(&mut body)?;
+
+    match create_json_file(args[1].as_str(), &body) {
+        Ok(_) => println!("Successfully created"),
+        Err(e) => eprintln!("Error: {}", e),
+    };
+
+    println!("status: {}", res.status());
+    println!("header: {:?}", res.headers());
+    println!("Body: \n{}", body);
+    Ok(())
 }
