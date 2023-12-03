7df6fcc46076328bb7350c5eff9d66faec21183b
PsychoPunkSage <abhinav.prakash319@gmail.com>
2023-12-03 16:30:55 +0530

    feat: json_write in file ready

--- json_writer/src/main.rs
+++ json_writer/src/main.rs
@@ -1,3 +1,48 @@
+use serde::{Serialize, Deserialize};
+use std::fs::File;
+use std::io::Write;
+
+#[derive(Serialize, Deserialize)]
+struct Paragraph {
+    name: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Article {
+    article: String,
+    author: String,
+    paragraphs: Vec<Paragraph>,
+}
+
+fn create_json_file(path: &str, content: String) -> Result<(), std::io::Error> {
+    let mut file = File::create(path)?;
+    file.write_all(content.as_bytes())?;
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let article = Article {
+        article: String::from("Struct to JSON write"),
+        author: String::from("PsychoPunkSage"),
+        paragraphs: vec![
+            Paragraph{
+                name: String::from("Para 1"),
+            }, 
+            Paragraph{
+                name: String::from("Para 2"),
+            },
+            Paragraph{
+                name: String::from("Para 2"),
+            }
+        ],
+    };
+
+    // ----Main----\\
+    let json = serde_json::to_string(&article).unwrap();
+    println!("_______\n|JSON:|\n-------\n {}", json);
+
+    match create_json_file("./test.json", json) {
+        Ok(_) => println!("Successfully created"),
+        Err(error) => println!("Error: {}", error),
+    };
 }
