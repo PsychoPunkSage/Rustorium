cf8ea2729ea820b0f698ee2620efc98526a3452f
PsychoPunkSage <abhinav.prakash319@gmail.com>
2023-12-03 15:53:26 +0530

    feat: json_reader is ready

--- json_reader/src/main.rs
+++ json_reader/src/main.rs
@@ -1,3 +1,58 @@
+use serde::{Deserialize, Serialize};
+use std::fs;
+use std::io;
+
+#[derive(Serialize, Deserialize)]
+struct Paragraph {
+    name: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Article {
+    article: String,
+    author: String,
+    paragraphs: Vec<Paragraph>, // Multiple Structs of "Paragraphs"
+}
+
+fn read_json(raw_json: &str) -> Article {
+    let parsed = serde_json::from_str(raw_json).unwrap();
+    return parsed;
+}
+
+// fn read_json_file(path: &str) -> Result<String, std::io::Error> {
+//     let mut file = fs::File::open(path)?;
+//     let mut contents = String::new();
+
+//     file.read_to_string(&mut contents)?;
+//     Ok(contents);
+// }
+
 fn main() {
-    println!("Hello, world!");
+    let json = r#"
+    {
+        "article": "Work with JSON on Rust",
+        "author": "PsychoPunkSage",
+        "paragraphs": [
+            {
+                "name": "A paragraph"
+            },
+            {
+                "name": "Paragraph 1"
+            },
+            {
+                "name": "Paragraph 2"
+            },
+            {
+                "name": "Paragraph 3"
+            }
+        ]
+    }"#;
+
+    let parsed: Article = read_json(json);
+    for line in 0..parsed.paragraphs.len() {
+        println!(
+            "\nPara:{} => Para is: {}",
+            line, parsed.paragraphs[line].name
+        );
+    }
 }
