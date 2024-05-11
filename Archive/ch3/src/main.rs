f433f9b1e524967fb841adbeac634e3d2d910316
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-05-11 23:37:09 +0530

    fix::> compound types added

--- Archive/ch3/src/main.rs
+++ Archive/ch3/src/main.rs
@@ -15,4 +15,45 @@ fn main() {
 
     let length = "abhdnfl pqpe";
     println!("length of 'length': {}", length.len());
+
+    // ||||||||||
+    // Data Types
+    // ||||||||||
+
+    let a = 98_222;
+    let b = 0xff; // Hex
+    let c = 0o77; // Octadecimal
+    let d = 0b1111_0000; // Binary
+    let e = b'A'; // Bytes -> u8 ONLY
+    println!("a: {}", a);
+    println!("b: {}", b);
+    println!("c: {}", c);
+    println!("d: {}", d);
+    println!("e: {}", e);
+
+    // |||||||||||||||
+    // Compound Types
+    // |||||||||||||||
+
+    let tup = ("Yo", 90);
+    let (stri, num) = tup; // Destructutring
+    let num = tup.1; // index starts from `0`
+
+    let errors = [400, 404, 500];
+    let not_found = errors[1];
+    let bytes = [0; 8]; // Create an array of len=8 with all values=0
+
+    // if-else inside `let`
+    let condition = true;
+    let number = if condition { 3 } else { 90 };
+
+    // loop inside `let`
+
+    let mut counter = 0;
+    let a = loop {
+        counter += 1;
+        if counter == 11 {
+            break counter * 2;
+        }
+    };
 }
