ddfef95163f15118a6c83af7e75931c82d437fc8
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-05-12 11:05:59 +0530

    fix::> Structs tuple structs done

--- Archive/ch5_structs/src/main.rs
+++ Archive/ch5_structs/src/main.rs
@@ -6,6 +6,11 @@ struct User {
     active: bool,
 }
 
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
     let mut user1 = User {
         username: String::from("user1"),
@@ -27,6 +32,22 @@ fn main() {
 
     let user2 = make_user(String::from("APPA"), String::from("ap@ap.com"));
     println!("{:?}", user2);
+
+    // Importing data from other user
+    let user3 = User {
+        username: String::from("user3"),
+        email: String::from("user3@example.com"),
+        ..user2 // Importing remaining data from `User2`
+    };
+
+    println!("{:?}", user3);
+
+    // TUPLE Structs
+    let rect = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    println!("Area: {} px", find_area_of_rect(rect));
 }
 
 fn make_user(username: String, email: String) -> User {
@@ -36,4 +57,8 @@ fn make_user(username: String, email: String) -> User {
         sign_in_count: 0,
         active: false,
     }
+}
+
+fn find_area_of_rect(dimensions: Rectangle) -> u32 {
+    dimensions.height * dimensions.width
 }
