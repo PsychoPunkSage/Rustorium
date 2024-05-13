3fd9929c4a045f57609daa0bb131b6e1aa857e04
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-05-13 13:31:54 +0530

    fix::> Basic done of collections

--- Archive/ch8_collection/src/main.rs
+++ Archive/ch8_collection/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let a = [1, 2, 3];
+    let a = [1, 2, 3, 4, 5, 6];
     let mut v: Vec<i32> = Vec::new();
     v.push(1);
     v.push(2);
@@ -14,5 +14,23 @@ fn main() {
     match v.get(3) {
         Some(ele) => println!("4th element {}", ele),
         None => println!("No third element"),
+    }
+
+    // Parsing a vector
+    for i in a {
+        println!("{}", i);
+    }
+
+    for ji in &mut a.iter() {
+        println!("{}", *ji + 50);
+    }
+
+    for j in a.iter() {
+        println!("{}", *j);
+    }
+
+    for (k, l) in a.iter().enumerate() {
+        // k -> index || l -> element reference
+        println!("{} {}", k, l);
     }
 }
