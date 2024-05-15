f1585937325f0bea839c5acfbcf9f6e5fe7dc4c5
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-05-15 23:13:37 +0530

    fix::> generic done

--- Archive/ch10_1_generic/src/main.rs
+++ Archive/ch10_1_generic/src/main.rs
@@ -1,3 +1,5 @@
+// At runtime RUST will auto-generate / compile Generic for different cases.
+// i.e. Point<i32, i32>, Point<&str, char> Point<f64, f64> Point<f64, i32>
 struct Point<T, U> {
     X: T,
     Y: U,
@@ -12,10 +14,17 @@ impl<T, U> Point<T, U> {
     fn y(&self) -> &U {
         &self.Y
     }
+
+    fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
+        Point {
+            X: self.X,
+            Y: other.Y,
+        }
+    }
 }
 
 // Concrete Impl block
-impl<U> Point<f64, U> {
+impl<U> Point<U, f64> {
     fn y_corr(&self) -> f64 {
         self.Y
     }
@@ -29,8 +38,12 @@ fn main() {
 
     // Struct with generic
     let p1 = Point { X: 0, Y: 10 };
-    let p2 = Point { X: 12.89, Y: 10.10 };
-    let p2 = Point { X: 12.89, Y: 10 };
+    let p2 = Point { X: "Hello", Y: 'c' };
+    let p3 = Point { X: 12.89, Y: 10.10 };
+    let p4 = Point { X: 12.89, Y: 10 };
+
+    let p_t = p1.mixup(p2);
+    println!("V_mix: {} || Y_mix: {}", p_t.X, p_t.Y);
 }
 
 fn get_largest_val<T: PartialOrd + Copy>(list: Vec<T>) -> T {
