26952d9b5cdf1820603b3cb41f23029d82848892
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-05-15 23:23:01 +0530

    fix::> Impl traits samples

--- Archive/ch10_2_trait/src/main.rs
+++ Archive/ch10_2_trait/src/main.rs
@@ -1,3 +1,30 @@
+pub trait Summary {
+    fn summarize(&self) -> String;
+}
+pub struct NewArticle {
+    pub author: String,
+    pub headline: String,
+    pub content: String,
+}
+
+impl Summary for NewArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {}", self.headline, self.author)
+    }
+}
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}, by {}", self.content, self.username)
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
