8356d2defbf186c7377479a8592363c64f31c642
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-05-18 23:56:31 +0530

    fix::> added USAGE for Box

--- Archive/ch15_smart_pointers/src/main.rs
+++ Archive/ch15_smart_pointers/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    // `Box` smart-pointer allocate memory in "HEAP"
+    // a `pointer` to the data is stored in stack
+
+    /*
+    USAGE:
+    - Don't know the size of data @compile time
+    - Want to transfer the ownership of large amount of data.
+     */
+    let b = Box::new(12);
+    println!("b::> {}", b);
 }
