53d1e941ed024e1788b21d0df6fa3d8b932312d8
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-05-20 23:56:01 +0530

    fix::> RC done

--- Archive/ch15_4_ref_counting/src/main.rs
+++ Archive/ch15_4_ref_counting/src/main.rs
@@ -1,1 +1,20 @@
-fn main() {}
+use std::rc::Rc;
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
+
+fn main() {
+    // For multiple Ownership :: only for SINGLE Treaded Program
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("Count after creating a = {}", Rc::strong_count(&a));
+    let b = Cons(3, Rc::clone(&a)); // .clone ::> Increases the reference count.
+    println!("Count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("Count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("Count after creating c = {}", Rc::strong_count(&a));
+}
