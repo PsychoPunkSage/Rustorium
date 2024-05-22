efd3e78c6e9b2e81cb7e39370fb8e3fb9090ed02
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-05-22 23:39:07 +0530

    fix::> using lib.rs inside main.rs

--- Archive/ch17_2_trait_objects/src/main.rs
+++ Archive/ch17_2_trait_objects/src/main.rs
@@ -1,1 +1,35 @@
-fn main() {}
+use ch17_2_trait_objects::{Button, Draw, Screen};
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        println!("Select obj DONE")
+    }
+}
+
+fn main() {
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 10,
+                height: 13,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No"),
+                ],
+            }),
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                label: String::from("OK"),
+            }),
+        ],
+    };
+    screen.run();
+}
