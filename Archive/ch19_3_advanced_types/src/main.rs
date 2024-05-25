3b70cf481d46ddcde7f2783b84dfa94396e1d95b
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-05-25 16:02:30 +0530

    fix::> advancced types DOne

--- Archive/ch19_3_advanced_types/src/main.rs
+++ Archive/ch19_3_advanced_types/src/main.rs
@@ -10,4 +10,43 @@ fn main() {
     println!("x + y = {}", x + y);
 
     // Type Alias
+    type Thunk = Box<dyn Fn() + Send + 'static>;
+
+    let f: Thunk = Box::new(|| println!("hi"));
+
+    fn takes_thunk(f: Thunk) {
+        // --snip --
+    }
+
+    fn returns_thunk() -> Thunk {
+        // --snip --
+    }
+
+    // NEVER type
+    // fn never_return() -> ! {}
+    // Ex
+    let game = true;
+    let mut guess = String::new();
+    while game {
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue, // `continue` never_return.
+        };
+    }
+
+    let none: Option<i32> = None;
+    let a = match none {
+        Some(val) => val,
+        None => panic!("Panic `NEVER` return anything"),
+    };
+
+    loop {
+        println!("Loop NEVER return anything UNLESS Break statement is introduced");
+    }
+}
+
+// Generic TRAITS ALWAYS implement `Sized` feature... which ensures that memory size of object is known at compile time.
+fn generic<T: ?Sized>(t: &T) {
+    // ? -> may/may not be `Sized` type
+    // Since T can potentially be UN-Sized So, the argument type is put behind REFERENCE
 }
