ba25fc0c538598d8bf949346cf31e30674c50322
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-05-25 15:26:22 +0530

    fix::> typecasting a type DONE

---
+++ Archive/ch19_3_advanced_types/src/main.rs
@@ -0,0 +1,13 @@
+struct Age(u32);
+struct ID(u32);
+
+fn main() {
+    type Kilometers = i32;
+
+    let x: i32 = 109;
+    let y: Kilometers = 109;
+
+    println!("x + y = {}", x + y);
+
+    // Type Alias
+}
