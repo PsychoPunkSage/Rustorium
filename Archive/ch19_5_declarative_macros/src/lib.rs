0af049426bd93bf40e4e6aa5c1da631e0646dd0d
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-05-26 10:18:03 +0530

    fix::> Declarative macros def done

--- Archive/ch19_5_declarative_macros/src/lib.rs
+++ Archive/ch19_5_declarative_macros/src/lib.rs
@@ -1,1 +1,24 @@
-
+#[macro_export]
+macro_rules! vec {
+    /*
+    Expression Matching:
+    - $x:expr ::> Store any expression that comes inside $x
+    - ,       ::> A literal ,(comma) may come while parsing the expression.
+    - *       ::> Means pattern may match more than 0(zero) times.
+    */
+    ( $( $x:expr ),* ) => {
+        {
+            let mut temp_vec = Vec::new();
+            /* Generate `temp_vec.push($x)` for every match we get.
+               Something like this:
+                - temp_vec.push(1);
+                - temp_vec.push(2);
+                - temp_vec.push(3);
+            */
+            $(
+                temp_vec.push($x);
+            )*
+            temp_vec
+        }
+    };
+}
