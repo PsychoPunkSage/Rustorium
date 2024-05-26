e655a4b059ec190ea6c5a65bca231a537195bc94
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-05-26 12:18:45 +0530

    fix:::> impl of procedural macro done

---
+++ Archive/ch19_6_procedural_macros/src/lib.rs
@@ -0,0 +1,4 @@
+// use proc_macro;
+
+// #[some_attribute] // specify the kind of `procedural macro` we are creating.
+// pub fn some_name(input: TokenStream) -> TokenStream {}
--- Archive/ch19_6_procedural_macros/src/main.rs
+++ Archive/ch19_6_procedural_macros/src/main.rs
@@ -1,3 +1,20 @@
+// use ::HelloMacro;
+// use hello_macro_derive::HelloMacro;
+use ch19_6_hello_macro::HelloMacro;
+use ch19_6_hello_macro_derive::HelloMacro;
+
+#[derive(HelloMacro)]
+struct Pancakes;
+
 fn main() {
-    println!("Hello, world!");
+    /*
+    Procedural Macro:
+    = Take code as input, work on that code and produce code as output.
+    ** Types
+    - Custom derive
+    - Attribute like
+    - Function like
+     */
+
+    Pancakes::hello_macro();
 }
