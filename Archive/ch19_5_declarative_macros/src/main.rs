452b9fafad97f931f89ffd43c409cb6df2aa3519
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-05-26 10:18:31 +0530

    fix::> macro custom def used

--- Archive/ch19_5_declarative_macros/src/main.rs
+++ Archive/ch19_5_declarative_macros/src/main.rs
@@ -1,4 +1,8 @@
+/*
+IMP. BOOK for MACROS ::> https://veykril.github.io/tlborm/
+*/
+
 fn main() {
-    let v1 = vec![1, 2, 3, 4, 5];
-    let v2 = vec!['a', 'b', 'c', 'd', 'e', 'f'];
+    let v1: Vec<u32> = vec![1, 2, 3, 4, 5];
+    let v2: Vec<char> = vec!['a', 'b', 'c', 'd', 'e', 'f'];
 }
