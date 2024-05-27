5f744c58a0f7083643615116c0c544a0ce7ee488
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-05-27 17:34:15 +0530

    fix::> Basic impl done

--- Archive/ch20_2_multi_threaded_web_server/src/lib.rs
+++ Archive/ch20_2_multi_threaded_web_server/src/lib.rs
@@ -1,1 +1,16 @@
 pub struct ThreadPool;
+
+impl ThreadPool {
+    pub fn new(threads: u32) -> ThreadPool {
+        ThreadPool
+    }
+
+    // same as impl of thread::spawn
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce(),       // takes ownership of value and envs
+        F: Send + 'static, // Send:: transfer threads from one thread to another || 'static :: receiver can hold on to the value as long as they want.
+    {
+        // Builder::new().spawn(f).expect("failed to spawn thread")
+    }
+}
