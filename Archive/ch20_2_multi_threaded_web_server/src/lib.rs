03cb2e030a2e76d8aff1f8e14127523c7e6e78e5
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-05-27 18:14:33 +0530

    fix::> Done the implementation of ThreadPool along with job assigning

--- Archive/ch20_2_multi_threaded_web_server/src/lib.rs
+++ Archive/ch20_2_multi_threaded_web_server/src/lib.rs
@@ -1,8 +1,70 @@
-pub struct ThreadPool;
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread,
+};
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        Worker {
+            id,
+            thread: thread::spawn(move || loop {
+                // loop:: cause we want our `thread` to constantly look for JOBS
+                let job = receiver.lock().unwrap().recv().unwrap();
+                println!("WORKER:{} assigned a JOB; executing....", id);
+                job();
+            }),
+        }
+    }
+}
+
+// Holds on the `closures` that we want to send down the `channel`.
+// Wee need ALL TYPES OF JOBS to get executed
+type Job = Box<dyn FnOnce() + Send + 'static>;
 
 impl ThreadPool {
-    pub fn new(threads: u32) -> ThreadPool {
-        ThreadPool
+    /// Create a new thread pool
+    ///
+    /// the `num_threads` is the number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the `num_threads` is 0 or less.
+    pub fn new(num_threads: usize) -> ThreadPool {
+        assert!(num_threads > 0);
+
+        let (sx, rx) = mpsc::channel();
+        let rx = Arc::new(Mutex::new(rx));
+
+        let mut workers = Vec::with_capacity(num_threads);
+        for id in 0..num_threads {
+            // create threads
+            /*
+            We will create another structs called `WORKER`
+
+            => Thread spawning will implement a `closure` immediately after thread creation...  we don't want that...
+            => We want the thread to only execute when it is assigned some work
+
+            ***
+            => We want workers to have shared ownership of `rx`
+                * Arc   :: Thread-safe multiple ownership
+                * Mutex :: Thread-safe mutability
+             */
+            workers.push(Worker::new(id, Arc::clone(&rx)));
+        }
+
+        ThreadPool {
+            workers,
+            sender: sx,
+        }
     }
 
     // same as impl of thread::spawn
@@ -11,6 +73,10 @@ impl ThreadPool {
         F: FnOnce(),       // takes ownership of value and envs
         F: Send + 'static, // Send:: transfer threads from one thread to another || 'static :: receiver can hold on to the value as long as they want.
     {
-        // Builder::new().spawn(f).expect("failed to spawn thread")
+        // We want to sen closures to different threads
+        // Best way:: Channels
+
+        let job = Box::new(f);
+        self.sender.send(job).unwrap();
     }
 }
