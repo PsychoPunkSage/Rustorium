b44d89d31de6469f514b03afded03ce28841ed36
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-05-27 16:49:19 +0530

    fix::> Implemnted single thread web server

--- Archive/ch20_1_single_threaded_web_server/src/main.rs
+++ Archive/ch20_1_single_threaded_web_server/src/main.rs
@@ -1,3 +1,169 @@
+use std::fs;
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = match TcpListener::bind("127.0.0.1:7878") {
+        Ok(listener) => listener,
+        Err(e) => panic!("Could not bind to port:\nError{}", e),
+    };
+
+    for stream in listener.incoming() {
+        // .incoming() ::> iterator over the connections being received on this listener
+        let stream = match stream {
+            Ok(st) => st,
+            Err(e) => {
+                println!("Connection refused");
+                panic!("Could not accept connection:\nError{}", e)
+            }
+        };
+
+        // println!("Connection Established!!\n{:?}", steam);
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    // Buffer to hold data that is being read.
+    let mut buffer = [0; 1024];
+
+    let _ = match stream.read(&mut buffer) {
+        Ok(_) => {
+            // Check if the request is for the root path
+            let get = b"GET / HTTP/1.1\r\n";
+
+            if buffer.starts_with(get) {
+                // Serve index.html
+                let web_page = match fs::read_to_string("index.html") {
+                    Ok(string) => string,
+                    Err(e) => {
+                        println!("Could not read from file:\nError{}", e);
+                        return;
+                    }
+                };
+
+                let resp = format!(
+                    "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+                    web_page.len(),
+                    web_page
+                );
+                stream.write(resp.as_bytes()).unwrap();
+                stream.flush().unwrap();
+            } else {
+                // Serve 404.html
+                let error_page = match fs::read_to_string("404.html") {
+                    Ok(string) => string,
+                    Err(e) => {
+                        println!("Could not read from file:\nError{}", e);
+                        return;
+                    }
+                };
+
+                let status_line = "HTTP/1.1 404 NOT FOUND";
+                let resp = format!(
+                    "{}\r\nContent-Length: {}\r\n\r\n{}",
+                    status_line,
+                    error_page.len(),
+                    error_page
+                );
+                stream.write(resp.as_bytes()).unwrap();
+                stream.flush().unwrap();
+            }
+        }
+        Err(e) => {
+            println!("Could not read from stream:\nError{}", e);
+            return;
+        }
+    };
+    // let get = b"GET / HTTP/1.1\r\n";
+
+    // if buffer.starts_with(get) {
+    //     let _ = match stream.read(&mut buffer) {
+    //         Ok(_) => {
+    //             // println!("Request: {}", String::from_utf8_lossy(&buffer[..]))
+    //             let web_page = match fs::read_to_string("index.html") {
+    //                 Ok(string) => string,
+    //                 Err(e) => {
+    //                     println!("Could not read from file:\nError{}", e);
+    //                     return;
+    //                 }
+    //             };
+
+    //             let resp = format!(
+    //                 "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+    //                 web_page.len(),
+    //                 web_page
+    //             );
+    //             stream.write(resp.as_bytes()).unwrap();
+    //             stream.flush().unwrap();
+    //         }
+    //         Err(e) => {
+    //             println!("Could not read from stream:\nError{}", e);
+    //             return;
+    //         }
+    //     };
+    // } else {
+    //     // 404 page
+    //     let _ = match stream.read(&mut buffer) {
+    //         Ok(_) => {
+    //             let error_page = match fs::read_to_string("index.html") {
+    //                 Ok(string) => string,
+    //                 Err(e) => {
+    //                     println!("Could not read from file:\nError{}", e);
+    //                     return;
+    //                 }
+    //             };
+
+    //             let status_line = "HTTP/1.1 404 NOT FOUND";
+    //             let resp = format!(
+    //                 "{}\r\nContent-Length: {}\r\n\r\n{}",
+    //                 status_line,
+    //                 error_page.len(),
+    //                 error_page
+    //             );
+    //             stream.write(resp.as_bytes()).unwrap();
+    //             stream.flush().unwrap();
+    //         }
+    //         Err(e) => {
+    //             println!("Could not read from stream:\nError{}", e);
+    //             return;
+    //         }
+    //     };
+    // }
+    // // // stream.read(&mut buffer).unwrap();
+    // // // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    // // let _ = match stream.read(&mut buffer) {
+    // //     Ok(_) => {
+    // //         // println!("Request: {}", String::from_utf8_lossy(&buffer[..]))
+    // //         let web_page = match fs::read_to_string("index.html") {
+    // //             Ok(string) => string,
+    // //             Err(e) => {
+    // //                 println!("Could not read from file:\nError{}", e);
+    // //                 return;
+    // //             }
+    // //         };
+
+    // //         let error_page = match fs::read_to_string("index.html") {
+    // //             Ok(string) => string,
+    // //             Err(e) => {
+    // //                 println!("Could not read from file:\nError{}", e);
+    // //                 return;
+    // //             }
+    // //         };
+    // //     }
+    // //     Err(e) => {
+    // //         println!("Could not read from stream:\nError{}", e);
+    // //         return;
+    // //     }
+    // // };
+
+    // /*
+    // REQUEST Body:
+
+    // HTTP-Version Status-Code Reason-Phrase CRLF
+    // headers CRLF
+    // message-body
+
+    // ex: HTTP/1.1 200 OK\r\n\r\n
+    // */
 }
