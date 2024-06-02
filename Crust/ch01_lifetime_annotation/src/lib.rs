47621f29b51ecc6b606e534b7ef5ce9075717f4d
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-06-02 18:44:57 +0530

    fix::> Almost full program

--- Crust/ch01_lifetime_annotation/src/lib.rs
+++ Crust/ch01_lifetime_annotation/src/lib.rs
@@ -1,12 +1,20 @@
 // #![warn(missing_debug_implementations, missing_docs, rust_2018_idioms)]
 
-pub struct StrSplit {
-    remainder: &str,
-    delimiter: &str,
+#[derive(Debug, PartialEq)]
+pub struct StrSplit<'a> {
+    remainder: &'a str,
+    delimiter: &'a str,
 }
 
-impl StrSplit {
-    pub fn new(haystack: &str, delimiter: &str) -> Self {
+impl<'a> StrSplit<'a> {
+    // We can use `StrSplit` for as long as `input string` are valid !!!
+    /*
+    Me to Complier...
+        - I will give you `StrSplit` with lifetime 'a as long as you ensure `haystack` and `delimiter` is around for 'a lifetime
+
+    impl<'a> ::> this says that this impl is generic over this lifetime 'a...
+    */
+    pub fn new(haystack: &'a str, delimiter: &'a str) -> Self {
         Self {
             remainder: haystack,
             delimiter,
@@ -14,10 +22,15 @@ impl StrSplit {
     }
 }
 
-// let x: StrSplit;
-// for part in x {......}
-impl Iterator for StrSplit {
-    type Item = &str;
+/*
+// Compiler know this is wrong but it doesn't know the right answer
+fn multiply(x: (), y: i32) -> i32 {
+    x * y
+}
+*/
+
+impl<'a> Iterator for StrSplit<'a> {
+    type Item = &'a str;
 
     fn next(&mut self) -> Option<Self::Item> {
         if let Some(next_delim) = self.remainder.find(self.delimiter) {
@@ -28,6 +41,8 @@ impl Iterator for StrSplit {
             None
         } else {
             let rest = self.remainder;
+            // &'a str       &'static str
+            // We can assign a lifetime with `same type` of lifetime or `longer` lifetime. VICE VERSA not true.
             self.remainder = "";
             Some(rest)
         }
@@ -41,7 +56,9 @@ mod tests {
     #[test]
     fn it_works() {
         let haystack = "a b c d e f";
-        let letters = StrSplit::new(haystack, " ").collect();
-        assert_eq!(letters, vec!["a", "b", "c", "d", "e", "f"].into_iter())
+        let mut letters = StrSplit::new(haystack, " ");
+        println!("{:?}", letters.next());
+        assert!(letters.eq(vec!["a", "b", "c", "d", "e", "f"].into_iter()))
+        // assert_eq!(2 + 2, 3);
     }
 }
