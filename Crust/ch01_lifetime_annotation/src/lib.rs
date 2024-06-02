a01014de8f2a5fa16b3bdd2b6eea047c4c081392
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-06-02 17:43:09 +0530

    fix::> iternator impl done

--- Crust/ch01_lifetime_annotation/src/lib.rs
+++ Crust/ch01_lifetime_annotation/src/lib.rs
@@ -1,9 +1,17 @@
 // #![warn(missing_debug_implementations, missing_docs, rust_2018_idioms)]
 
-pub struct StrSplit {}
+pub struct StrSplit {
+    remainder: &str,
+    delimiter: &str,
+}
 
 impl StrSplit {
-    pub fn new(haystack: &str, needle: &str) -> Self {}
+    pub fn new(haystack: &str, delimiter: &str) -> Self {
+        Self {
+            remainder: haystack,
+            delimiter,
+        }
+    }
 }
 
 // let x: StrSplit;
@@ -11,7 +19,19 @@ impl StrSplit {
 impl Iterator for StrSplit {
     type Item = &str;
 
-    fn next(&mut self) -> Option<Self::Item> {}
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(next_delim) = self.remainder.find(self.delimiter) {
+            let until_delimiter = &self.remainder[..next_delim];
+            self.remainder = &self.remainder[(next_delim + self.delimiter.len())..];
+            Some(until_delimiter)
+        } else if self.delimiter.is_empty() {
+            None
+        } else {
+            let rest = self.remainder;
+            self.remainder = "";
+            Some(rest)
+        }
+    }
 }
 
 #[cfg(test)]
