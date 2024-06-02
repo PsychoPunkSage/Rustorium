a59637bbd8ca6c4a358e84ace8555a343dfcabf7
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-06-02 17:20:17 +0530

    fix::> Basic API done

--- Crust/ch01_strsplit/src/lib.rs
+++ Crust/ch01_strsplit/src/lib.rs
@@ -1,14 +1,27 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+// #![warn(missing_debug_implementations, missing_docs, rust_2018_idioms)]
+
+pub struct StrSplit {}
+
+impl StrSplit {
+    pub fn new(haystack: &str, needle: &str) -> Self {}
+}
+
+// let x: StrSplit;
+// for part in x {......}
+impl Iterator for StrSplit {
+    type Item = &str;
+
+    fn next(&mut self) -> Option<Self::Item> {}
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use crate::StrSplit;
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let haystack = "a b c d e f";
+        let letters = StrSplit::new(haystack, " ").collect();
+        assert_eq!(letters, vec!["a", "b", "c", "d", "e", "f"].into_iter())
     }
 }
