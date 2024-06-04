96136da8f864b5c0785086d80b5de6b607492a5f
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-06-04 21:09:18 +0530

    fix::> Normal push

--- Crust/ch03_iterators/src/main.rs
+++ Crust/ch03_iterators/src/main.rs
@@ -1,3 +1,19 @@
+trait Iterator {
+    // We used `Associated type` here cause,
+    // We expect the a particular `type` to have ONLY a single implementation.
+    // Associated types reduce the amount of extra generic type parameters you will be using.
+    type Item;
+    fn next(&mut self) -> Option<Self::Item>;
+}
+
+trait Service<Request> {
+    // Have different impl for a particular `type` of parameter.
+    fn doer(&mut self, request: Request);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut iter = vec![1, 2, 3, 4].into_iter();
+    while let Some(e) = iter.next() {
+        println!("{}", e);
+    }
 }
