65b5cfb0c0192ed7e1c31915a3b2861a98a428a1
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-06-09 23:36:38 +0530

    fix::> impl Ref and RefMut for RefCell

--- Crust/ch04_smart_pointers_internal_mutability/src/refcell.rs
+++ Crust/ch04_smart_pointers_internal_mutability/src/refcell.rs
@@ -22,26 +22,78 @@ impl<T> RefCell<T> {
         }
     }
 
-    pub fn borrow(&self) -> Option<&T> {
+    pub fn borrow(&self) -> Option<Ref<'_, T>> {
         match self.state.get() {
             RefState::Unshared => {
                 self.state.set(RefState::Shared(1));
-                Some(unsafe { &*self.value.get() })
+
+                Some(Ref { refcell: self })
             }
             RefState::Shared(n) => {
                 self.state.set(RefState::Shared(n + 1));
-                Some(unsafe { &*self.value.get() })
+                Some(Ref { refcell: self })
             }
             RefState::Exclusive => None,
         }
     }
 
-    pub fn borrow_mut(&self) -> Option<&mut T> {
+    pub fn borrow_mut(&self) -> Option<RefMut<'_, T>> {
         if let RefState::Unshared = self.state.get() {
+            // SAFETY: no other refernece have been given out since state would be Shared isntead of exclusive
             self.state.set(RefState::Exclusive);
-            Some(unsafe { &mut *self.value.get() })
+            Some(RefMut { refcell: self })
         } else {
             None
         }
+    }
+}
+
+pub struct Ref<'refcell, T> {
+    refcell: &'refcell RefCell<T>,
+}
+
+impl<T> std::ops::Deref for Ref<'_, T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        // SAFETY:
+        // a `Ref` is only created if no exclusive refernece have been given out
+        // once it is given out, state is set to Shared, so no exclusive refernece are given
+        // so dereferencing into a shared reference is possible.
+        unsafe { &*self.refcell.value.get() }
+    }
+}
+
+impl<T> Drop for Ref<'_, T> {
+    fn drop(&mut self) {
+        match self.refcell.state.get() {
+            RefState::Exclusive | RefState::Unshared => unreachable!(),
+            RefState::Shared(1) => self.refcell.state.set(RefState::Unshared),
+            RefState::Shared(n) => self.refcell.state.set(RefState::Shared(n - 1)),
+        }
+    }
+}
+
+pub struct RefMut<'refcell, T> {
+    refcell: &'refcell RefCell<T>,
+}
+
+impl<T> std::ops::Deref for RefMut<'_, T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        // SAFETY:
+        // a `Ref` is only created if no exclusive refernece have been given out
+        // once it is given out, state is set to Shared, so no exclusive refernece are given
+        // so dereferencing into a shared reference is possible.
+        unsafe { &*self.refcell.value.get() }
+    }
+}
+
+impl<T> std::ops::DerefMut for RefMut<'_, T> {
+    fn deref_mut(&mut self) -> &mut T {
+        // SAFETY:
+        // a `Ref` is only created if no exclusive refernece have been given out
+        // once it is given out, state is set to `Exclusive`, so no future refernece are given
+        // so dereferencing into a shared reference is possible.
+        unsafe { &mut *self.refcell.value.get() }
     }
 }
