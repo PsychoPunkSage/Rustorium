0dc200622d283dcfe44ccb14a0953d29dfb5b9b3
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-07-11 23:54:01 +0530

    fix::> def cleared

--- Crust/ch07_async_await/src/main.rs
+++ Crust/ch07_async_await/src/main.rs
@@ -1,3 +1,23 @@
+#[allow(dead_code, unused_variables)]
+
 fn main() {
     println!("Hello, world!");
+
+    // type(x) != usize
+    // .await will return something which will eventually convert to `usize`.
+    let x = foo();
 }
+
+async fn foo() -> usize {
+    0
+}
+
+/*
+
+async fn foo() -> usize {}
+
+fn foo() -> impl Future<Output = usize> {
+    async {}
+}
+
+*/
