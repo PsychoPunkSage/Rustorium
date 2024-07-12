eaaae16e197fb772973606f37932a0fa8a95056d
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-07-12 11:47:14 +0530

    fix::> basics of tokio KQ EPOLL

--- Crust/ch07_async_await/src/main.rs
+++ Crust/ch07_async_await/src/main.rs
@@ -1,7 +1,7 @@
 use std::{future::Future, io::BufRead};
+use tokio;
 
 #[allow(dead_code, unused_variables)]
-
 /*
 Future::>
     - descrive a series of steps that will happen at some point in the future.
@@ -9,56 +9,65 @@ Future::>
 */
 
 fn main() {
-    println!("Hello, world!");
+    /*
+    Below Runtime is derived version of
+    #[tokio::main]
+    async fn main() {....}
+     */
+    let runtime = tokio::runtime::Runtime::new();
+    runtime.block_on(async {
+        println!("Hello, world!");
 
-    // If NOT using async/await
-    let read_from_terminal = std::thread::spawn(move || {
-        let mut x = std::io::Stdin::lock(&std::io::stdin());
-        for lines in x.lines() {
-            // Do something
+        // If NOT using async/await
+        let read_from_terminal = std::thread::spawn(move || {
+            let mut x = std::io::Stdin::lock(&std::io::stdin());
+            for lines in x.lines() {
+                // Do something
+            }
+        });
+
+        let read_from_network = std::thread::spawn(move || {
+            let mut x = std::net::TcpListener::bind("0.0.0.0:8000").unwrap();
+            while let Ok(stream) = x.accept() {
+                // Do something on stream
+                // Create `handle_connection` for all the stream <So you don't have to wait for slow client and can serve FAST client>
+            }
+        });
+
+        // USING Async
+        let network = read_from_networks();
+        let terminal = read_from_terminals();
+        let mut foo = foo2(_);
+
+        loop {
+            select! {
+                // This macro in present in various library.
+                // WAITS on various FUTURES....
+                // and tells which one got executed first.
+
+                stream <- network.await => {
+                    // do something on stream
+                }
+
+                line <- terminal.await => {
+                    // do something with line
+                }
+
+                foo <- foo.await => {
+                    // 1. Wait on `file1` reading :: yield happens
+                    // 2. control moves to the Top of call stack (i.e. select{})
+                    // 3. then select will again check for any progress in `stream` or `line`
+                    //      - if any of them changes; then run the required codes.
+                    //      - if nothing changes then continue Awaiting `Foo2``
+                    // 4. after this control shifts back to the bottom of the call stack.
+                }
+            }
         }
     });
-
-    let read_from_network = std::thread::spawn(move || {
-        let mut x = std::net::TcpListener::bind("0.0.0.0:8000").unwrap();
-        while let Ok(stream) = x.accept() {
-            // Do something on stream
-            // Create `handle_connection` for all the stream <So you don't have to wait for slow client and can serve FAST client>
-        }
-    });
-
-    // USING Async
-    let network = read_from_networks();
-    let terminal = read_from_terminals();
-
-    loop {
-        select! {
-            // This macro in present in various library.
-            // WAITS on various FUTURES....
-            // and tells which one got executed first.
-
-            stream <- network.await => {
-                // do something on stream
-            }
-
-            line <- terminal.await => {
-                // do something with line
-            }
-
-            foo <- foo2.await => {
-                // 1. Wait on `file1` reading :: yield happens
-                // 2. control moves to the Top of call stack (i.e. select{})
-                // 3. then select will again check for any progress in `stream` or `line`
-                //      - if any of them changes; then run the required codes.
-                //      - if nothing changes then continue Awaiting `Foo2``
-                // 4. after this control shifts back to the bottom of the call stack.
-            }
-        }
-    }
 
     // type(x) != usize
     // .await will return something which will eventually convert to `usize`.
-    let x = foo2();
+    // let x = foo2();
 }
 
 async fn foo1() -> usize {
@@ -70,7 +79,7 @@ async fn read_to_strings(_: &str) {}
 async fn read_from_networks() {}
 async fn read_from_terminals() {}
 
-fn foo2() -> impl Future<Output = usize> {
+fn foo2(cancel: tokio::sync::mpsc::Receiver<()>) -> impl Future<Output = usize> {
     async {
         println!("Inside Foo2");
         read_to_strings("file1").await;
@@ -96,7 +105,15 @@ fn foo2() -> impl Future<Output = usize> {
         println!("Inside Foo2");
         read_to_strings("file2").await;
         println!("Inside Foo2");
-        read_to_strings("file3").await;
+        // To Drop await.
+        race! {
+            done <- read_to_strings("file3").await {
+                // continue your job....
+            }
+            cancel <- cancel.await {
+                return 0;
+            }
+        }
         println!("Inside Foo2");
         read_to_strings("file4").await;
         0
