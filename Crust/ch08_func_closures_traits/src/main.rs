7422e62081304231b0a74dda1394a1cc62a0da85
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-07-16 11:42:45 +0530

    fix::> func item vs func pointer

--- Crust/ch08_func_closures_traits/src/main.rs
+++ Crust/ch08_func_closures_traits/src/main.rs
@@ -15,9 +15,25 @@ fn main() {
     Funtion Item:
         - "Zero sized" value that is only carried around at compile time the reference a unique function.
         - Its just an `Identifier`.
+        - it uniquely identifies a particular instance of a function.
+
+    Function Pointer:
+        - pointer to a function with a given signature.
     */
+    bazz(bar1::<i32>);
+    bazz(bar1::<u32>);
+
+    // `Function Item` & `Function Pointer` are different from one another. BUT `Function Item(s)`  are coersible inside the `Function Pointer`.
+    // Demonstrated above.
 }
 
 fn bar() {}
-fn bar1<T>() {}
+fn bar1<T>(_: u32) -> u32 {
+    0
+}
 async fn bar2() {}
+
+fn bazz(f: fn(u32) -> u32) {
+    // Now here the `function item` is properly converted to `fn pointer`.
+    println!("{}", std::mem::size_of_val(&f));
+}
