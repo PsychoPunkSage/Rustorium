35c68d769a0cf66a5ee8d73e65a8f97110df25e6
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-07-28 17:42:09 +0530

    fix::> compiler_fence docs added

--- Crust/ch09_atomics_memory_ordering/src/thread_sanitizer.rs
+++ Crust/ch09_atomics_memory_ordering/src/thread_sanitizer.rs
@@ -9,3 +9,11 @@ LOOM:
     - It takes concurrent program, instrument it, it will run it multiple times, And everytime we do a load through one of the loom DS, it will feed us back one of the possible Legal vals.
     - When stored; it will keep track of all the values that have been stored so that on other loads, it will expose the execution to one of the possible loads.
 */
+/*
+COMPILER FENCE:
+    - way to ensure that the compiler won't move a given loader store.
+    - SIMPLER: Compiler is not allowed to move an operation under the fence to above a fence / above a fence to below a fence within that thread execution.
+    - ONLY FOR COMPILERS, CPU can still execute things out of order.
+    - RARELY used
+    Mostly used to prevent a thread from Racing itself. -> This will only happen when using Signal Handlers.
+*/
