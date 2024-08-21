bccd7966655886ca1d3bb63a9e520b0ec356aa2c
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-08-21 15:59:00 +0530

    fix::> analysis of Send and Sync

--- Crust/ch06_send_sync/src/bin.rs
+++ Crust/ch06_send_sync/src/bin.rs
@@ -90,3 +90,14 @@ fn main() {
         }
     }
 }
+
+/*
+SEND:
+    - Something can safely be Send unless it shares mutable state with something else without enforcing exclusive access to it.
+    - Since `Carton` is not giving `mut reference`, so we're good
+    - OR, Carton holds a unique pointer to some data on the heap. This means that each Carton instance has its own pointer, and no other Carton or part of the program can access that same pointer unless it's explicitly given. ecause the pointer is unique, there’s no risk of two threads accidentally modifying the same data at the same time.
+
+SYNC:
+    - In the case of Carton, you need a mutable reference (&mut Carton) to write to the data. Rust's borrow checker ensures that a mutable reference is exclusive—meaning only one thread can have that reference at a time.
+    -
+*/
