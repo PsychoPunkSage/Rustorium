71efcc250cd00d4731247b94d42ca9d77e470b29
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-09-16 21:12:31 +0530

    fix::> Added a binary search DONE

--- Exercism/32-binary-search/binary-search/src/lib.rs
+++ Exercism/32-binary-search/binary-search/src/lib.rs
@@ -10,8 +10,14 @@ pub fn find(array: &[i32], key: i32) -> Option<usize> {
         Some(mid)
     } else if init < key {
         // Search in the right half, adjust index by adding `mid + 1`
-        // return Some(mid + find(&array[mid..], key).unwrap());
         find(&array[mid + 1..], key).map(|index| mid + 1 + index)
+        /*
+        return Some(mid + find(&array[mid..], key).unwrap());
+            - If the recursive call returns None, unwrapping it (.unwrap()) will cause the program to panic
+
+        find(&array[mid + 1..], key).map(|index| mid + 1 + index)
+            - If the recursive call returns None, the map operation will also return None, thus avoiding a panic.
+        */
     } else {
         // Search in the left half
         find(&array[..mid], key)
