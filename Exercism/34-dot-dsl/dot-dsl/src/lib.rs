14f43d961133a516fcdafaa135396a3d5c941255
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-09-17 20:05:04 +0530

    fix::> 8 cases passed - major revamp

--- Exercism/34-dot-dsl/dot-dsl/src/lib.rs
+++ Exercism/34-dot-dsl/dot-dsl/src/lib.rs
@@ -3,27 +3,34 @@ pub mod graph {
 
     pub mod graph_items {
         pub mod edge {
+            use std::collections::HashMap;
+
             #[derive(Debug, Clone, PartialEq)]
-            pub struct Edge<'attr> {
+            pub struct Edge {
                 val1: String,
                 val2: String,
-                attrs: Vec<(&'attr str, &'attr str)>,
+                attrs: HashMap<String, String>,
             }
 
-            impl<'attr> Edge<'attr> {
+            impl Edge {
                 pub fn new(val1: &str, val2: &str) -> Self {
                     Edge {
                         val1: val1.to_string(),
                         val2: val2.to_string(),
-                        attrs: Vec::new(),
+                        attrs: HashMap::new(),
                     }
                 }
             }
 
-            impl<'attr> Edge<'attr> {
-                pub fn with_attrs(mut self, attr: &'attr [(&str, &str)]) -> Self {
-                    self.attrs.extend_from_slice(attr);
+            impl Edge {
+                pub fn with_attrs(mut self, attr: &[(&str, &str)]) -> Self {
+                    self.attrs
+                        .extend(attr.iter().map(|&(k, v)| (k.to_string(), v.to_string())));
                     self
+                }
+
+                pub fn attr(&self, key: &str) -> Option<&str> {
+                    self.attrs.get(key).map(|v| v.as_str())
                 }
             }
         }
@@ -52,13 +59,13 @@ pub mod graph {
         }
     }
     #[derive(Debug, Clone, PartialEq)]
-    pub struct Graph<'node, 'edge> {
+    pub struct Graph<'node> {
         pub nodes: Vec<crate::graph::graph_items::node::Node<'node>>,
-        pub edges: Vec<crate::graph::graph_items::edge::Edge<'edge>>,
+        pub edges: Vec<crate::graph::graph_items::edge::Edge>,
         pub attrs: HashMap<String, String>,
     }
 
-    impl<'node, 'edge> Graph<'node, 'edge> {
+    impl<'node> Graph<'node> {
         pub fn new() -> Self {
             Graph {
                 nodes: Vec::new(),
@@ -68,25 +75,27 @@ pub mod graph {
         }
     }
 
-    impl<'node, 'edge> Graph<'node, 'edge> {
+    impl<'node> Graph<'node> {
         pub fn with_nodes(mut self, nodes: &'node [crate::graph::graph_items::node::Node]) -> Self {
             self.nodes.extend_from_slice(nodes);
             self
         }
 
-        pub fn with_edges(mut self, edges: &'edge [crate::graph::graph_items::edge::Edge]) -> Self {
+        pub fn with_edges(mut self, edges: &[crate::graph::graph_items::edge::Edge]) -> Self {
             self.edges.extend_from_slice(edges);
             self
         }
 
         pub fn with_attrs(mut self, attrs: &[(&str, &str)]) -> Self {
-            // attrs
-            //     .iter()
-            //     .map(|&(a, b)| self.attrs.insert(&a, &b))
-            //     .collect::<>();
-            for &(key, value) in attrs {
-                self.attrs.insert(key.to_string(), value.to_string());
-            }
+            // for &(key, value) in attrs {
+            //     self.attrs.insert(key.to_string(), value.to_string());
+            // }
+            self.attrs.extend(
+                // extend::> Extends a collection with the contents of an iterator.
+                attrs
+                    .iter()
+                    .map(|&(key, value)| (key.to_string(), value.to_string())),
+            );
             self
         }
     }
--- Exercism/34-dot-dsl/dot-dsl/tests/dot-dsl.rs
+++ Exercism/34-dot-dsl/dot-dsl/tests/dot-dsl.rs
@@ -89,89 +89,89 @@ fn graph_with_one_attribute() {
     assert_eq!(graph.attrs, expected_attrs);
 }
 
-// #[test]
+#[test]
 // #[ignore]
-// fn graph_with_attributes() {
-//     let nodes = vec![
-//         Node::new("a").with_attrs(&[("color", "green")]),
-//         Node::new("c"),
-//         Node::new("b").with_attrs(&[("label", "Beta!")]),
-//     ];
+fn graph_with_attributes() {
+    let nodes = vec![
+        Node::new("a").with_attrs(&[("color", "green")]),
+        Node::new("c"),
+        Node::new("b").with_attrs(&[("label", "Beta!")]),
+    ];
 
-//     let edges = vec![
-//         Edge::new("b", "c"),
-//         Edge::new("a", "b").with_attrs(&[("color", "blue")]),
-//     ];
+    let edges = vec![
+        Edge::new("b", "c"),
+        Edge::new("a", "b").with_attrs(&[("color", "blue")]),
+    ];
 
-//     let attrs = vec![("foo", "1"), ("title", "Testing Attrs"), ("bar", "true")];
+    let attrs = vec![("foo", "1"), ("title", "Testing Attrs"), ("bar", "true")];
 
-//     let expected_attrs = HashMap::from([
-//         ("foo".to_string(), "1".to_string()),
-//         ("title".to_string(), "Testing Attrs".to_string()),
-//         ("bar".to_string(), "true".to_string()),
-//     ]);
+    let expected_attrs = HashMap::from([
+        ("foo".to_string(), "1".to_string()),
+        ("title".to_string(), "Testing Attrs".to_string()),
+        ("bar".to_string(), "true".to_string()),
+    ]);
 
-//     let graph = Graph::new()
-//         .with_nodes(&nodes)
-//         .with_edges(&edges)
-//         .with_attrs(&attrs);
+    let graph = Graph::new()
+        .with_nodes(&nodes)
+        .with_edges(&edges)
+        .with_attrs(&attrs);
 
-//     assert_eq!(
-//         graph.nodes,
-//         vec![
-//             Node::new("a").with_attrs(&[("color", "green")]),
-//             Node::new("c"),
-//             Node::new("b").with_attrs(&[("label", "Beta!")]),
-//         ]
-//     );
+    assert_eq!(
+        graph.nodes,
+        vec![
+            Node::new("a").with_attrs(&[("color", "green")]),
+            Node::new("c"),
+            Node::new("b").with_attrs(&[("label", "Beta!")]),
+        ]
+    );
 
-//     assert_eq!(
-//         graph.edges,
-//         vec![
-//             Edge::new("b", "c"),
-//             Edge::new("a", "b").with_attrs(&[("color", "blue")]),
-//         ]
-//     );
+    assert_eq!(
+        graph.edges,
+        vec![
+            Edge::new("b", "c"),
+            Edge::new("a", "b").with_attrs(&[("color", "blue")]),
+        ]
+    );
 
-//     assert_eq!(graph.attrs, expected_attrs);
-// }
+    assert_eq!(graph.attrs, expected_attrs);
+}
 
-// #[test]
+#[test]
 // #[ignore]
-// fn edges_store_attributes() {
-//     let nodes = vec![
-//         Node::new("a").with_attrs(&[("color", "green")]),
-//         Node::new("c"),
-//         Node::new("b").with_attrs(&[("label", "Beta!")]),
-//     ];
+fn edges_store_attributes() {
+    let nodes = vec![
+        Node::new("a").with_attrs(&[("color", "green")]),
+        Node::new("c"),
+        Node::new("b").with_attrs(&[("label", "Beta!")]),
+    ];
 
-//     let edges = vec![
-//         Edge::new("b", "c"),
-//         Edge::new("a", "b").with_attrs(&[("color", "blue"), ("fill", "darkblue")]),
-//     ];
+    let edges = vec![
+        Edge::new("b", "c"),
+        Edge::new("a", "b").with_attrs(&[("color", "blue"), ("fill", "darkblue")]),
+    ];
 
-//     let attrs = vec![("foo", "1"), ("title", "Testing Attrs"), ("bar", "true")];
+    let attrs = vec![("foo", "1"), ("title", "Testing Attrs"), ("bar", "true")];
 
-//     let graph = Graph::new()
-//         .with_nodes(&nodes)
-//         .with_edges(&edges)
-//         .with_attrs(&attrs);
+    let graph = Graph::new()
+        .with_nodes(&nodes)
+        .with_edges(&edges)
+        .with_attrs(&attrs);
 
-//     assert_eq!(
-//         graph.edges,
-//         vec![
-//             Edge::new("b", "c"),
-//             Edge::new("a", "b").with_attrs(&[("color", "blue"), ("fill", "darkblue")]),
-//         ]
-//     );
+    assert_eq!(
+        graph.edges,
+        vec![
+            Edge::new("b", "c"),
+            Edge::new("a", "b").with_attrs(&[("color", "blue"), ("fill", "darkblue")]),
+        ]
+    );
 
-//     assert_eq!(graph.edges[1].attr("color"), Some("blue"));
-//     assert_eq!(graph.edges[1].attr("fill"), Some("darkblue"));
-//     assert_eq!(graph.edges[1].attr("foo"), None);
-//     assert_eq!(graph.edges[0].attr("color"), None);
-//     assert_eq!(graph.edges[0].attr("fill"), None);
-//     assert_eq!(graph.edges[0].attr("foo"), None);
-// }
+    assert_eq!(graph.edges[1].attr("color"), Some("blue"));
+    assert_eq!(graph.edges[1].attr("fill"), Some("darkblue"));
+    assert_eq!(graph.edges[1].attr("foo"), None);
+    assert_eq!(graph.edges[0].attr("color"), None);
+    assert_eq!(graph.edges[0].attr("fill"), None);
+    assert_eq!(graph.edges[0].attr("foo"), None);
+}
 
 // #[test]
 // #[ignore]
