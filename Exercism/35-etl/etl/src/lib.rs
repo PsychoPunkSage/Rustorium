b7176024581cc503014b91448134029a7162b035
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-09-21 11:01:15 +0530

    fix::> etl solved

--- Exercism/35-etl/etl/src/lib.rs
+++ Exercism/35-etl/etl/src/lib.rs
@@ -1,3 +1,34 @@
 use std::collections::BTreeMap;
 
-pub fn transform(h: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {}
+pub fn transform(h: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
+    let mut result = BTreeMap::new();
+
+    for (key, value) in h {
+        for char in value {
+            let lower_case_char = char.to_ascii_lowercase();
+            if let Some(count) = result.get_mut(&lower_case_char) {
+                *count += key;
+            } else {
+                result.insert(lower_case_char, *key);
+            }
+        }
+    }
+
+    result
+}
+
+pub fn main() {
+    let input = input_from(&[(1, vec!['A'])]);
+
+    let expected = expected_from(&[('a', 1)]);
+
+    assert_eq!(expected, transform(&input));
+}
+
+fn input_from(v: &[(i32, Vec<char>)]) -> BTreeMap<i32, Vec<char>> {
+    v.iter().cloned().collect()
+}
+
+fn expected_from(v: &[(char, i32)]) -> BTreeMap<char, i32> {
+    v.iter().cloned().collect()
+}
