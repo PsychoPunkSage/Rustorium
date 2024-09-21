1c5b785c652710cae0b4a3ba10e1615fe92bb9e9
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-09-21 11:22:29 +0530

    fix::> etl Done

--- Exercism/35-etl/etl/src/lib.rs
+++ Exercism/35-etl/etl/src/lib.rs
@@ -1,13 +1,17 @@
 use std::collections::BTreeMap;
 
 pub fn transform(h: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
-    let mut result = BTreeMap::new();
+    // let mut result = BTreeMap::new();
 
-    h.iter().for_each(|(a, b)| {
-        b.iter().for_each(|c| {
-            result.insert(c.to_ascii_lowercase(), *a);
-        })
-    });
+    // h.iter().for_each(|(a, b)| {
+    //     b.iter().for_each(|c| {
+    //         result.insert(c.to_ascii_lowercase(), *a);
+    //     })
+    // });
 
-    result
+    // result
+
+    h.iter()
+        .flat_map(|(a, b)| b.iter().map(|&c| (c.to_ascii_lowercase(), *a)))
+        .collect()
 }
