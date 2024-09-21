ff0c5969743726322d29149faf390282181a988d
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-09-21 14:51:12 +0530

    fix::> hamming opyimized done

--- Exercism/37-hamming/hamming/src/lib.rs
+++ Exercism/37-hamming/hamming/src/lib.rs
@@ -1,19 +1,20 @@
 /// Return the Hamming distance between the strings,
 /// or None if the lengths are mismatched.
 pub fn hamming_distance(s1: &str, s2: &str) -> Option<usize> {
-    if s1.len() != s2.len() {
-        return None;
-    }
+    // if s1.len() != s2.len() {
+    //     return None;
+    // }
 
-    let mut size = 0;
-    let v1 = s1.chars().collect::<Vec<char>>();
-    let v2 = s2.chars().collect::<Vec<char>>();
+    // let mut size = 0;
+    // let v1 = s1.chars().collect::<Vec<char>>();
+    // let v2 = s2.chars().collect::<Vec<char>>();
 
-    for i in 0..s1.len() {
-        if v1[i] != v2[i] {
-            size += 1;
-        }
-    }
+    // for i in 0..s1.len() {
+    //     if v1[i] != v2[i] {
+    //         size += 1;
+    //     }
+    // }
 
-    Some(size)
+    // Some(size)
+    (s1.len() == s2.len()).then(|| s1.chars().zip(s2.chars()).filter(|&(a, b)| a != b).count())
 }
