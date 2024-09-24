93d1288788640ed0979a324442c83ee59ec10167
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-09-24 10:40:58 +0530

    fix::> palindrome-products test Done

--- Exercism/41-palindrome-products/palindrome-products/src/lib.rs
+++ Exercism/41-palindrome-products/palindrome-products/src/lib.rs
@@ -20,29 +20,59 @@ impl Palindrome {
     }
 }
 
-use rayon::prelude::*;
+use std::u64;
+
+// use rayon::prelude::*;
 pub fn palindrome_products(min: u64, max: u64) -> Option<(Palindrome, Palindrome)> {
-    let palindromes: Vec<u64> = (min..=max)
-        .into_par_iter() // Parallel iterator: as the entire thing is Parallel
-        .flat_map(|v| {
-            (v..=max)
-                .into_par_iter() // Parallel iterator from the rayon crate
-                .filter_map(move |i| {
-                    let product = i * v;
-                    if is_palindrome(product) {
-                        Some(product)
-                    } else {
-                        None
-                    }
-                })
-        })
-        .collect();
+    let mut max_p = None;
+    let mut min_p = None;
 
-    let min_palindrome = palindromes.iter().min().copied();
-    let max_palindrome = palindromes.iter().max().copied();
+    // (min..=max)
+    //     // .into_par_iter() // Parallel iterator: as the entire thing is Parallel
+    //     .for_each(|v| {
+    //         (v..=max)
+    //             // .into_par_iter() // Parallel iterator from the rayon crate
+    //             .for_each(move |i| {
+    //                 let product = i * v;
+    //                 if is_palindrome(product) {
+    //                     if product < min_p.unwrap_or(u64::MAX) {
+    //                         min_p = Some(product);
+    //                     }
+    //                     if product > max_p.unwrap_or(u64::MIN) {
+    //                         max_p = Some(product);
+    //                     }
+    //                 }
+    //             })
+    //     });
 
-    match (min_palindrome, max_palindrome) {
-        (Some(min), Some(max)) => Some((Palindrome(min), Palindrome(max))),
+    // match (min_p, max_p) {
+    //     (Some(min), Some(max)) => Some((Palindrome(min), Palindrome(max))),
+    //     _ => None,
+    // }
+
+    for i in min..=max {
+        if i % 10 == 0 {
+            continue;
+        };
+        for j in min..=max {
+            if j % 10 == 0 {
+                continue;
+            };
+            let p = i * j;
+            if is_palindrome(p) {
+                if p < min_p.unwrap_or(u64::MAX) {
+                    min_p = Some(p);
+                }
+                if p > max_p.unwrap_or(u64::MIN) {
+                    max_p = Some(p);
+                }
+            }
+        }
+    }
+    match (min_p, max_p) {
+        (Some(min), Some(max)) => {
+            Some((Palindrome::new(min).unwrap(), Palindrome::new(max).unwrap()))
+        }
         _ => None,
     }
 }
