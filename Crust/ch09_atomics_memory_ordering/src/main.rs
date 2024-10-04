73f891859d282e24f13f425037df85b5eb5f4aa2
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-10-04 23:46:52 +0530

    fix::> Doc changes

--- Crust/ch09_atomics_memory_ordering/src/main.rs
+++ Crust/ch09_atomics_memory_ordering/src/main.rs
@@ -2,14 +2,14 @@
 ATOMICUSIZE
     > designed for `thread-safe` operations. It's a wrapper around the underlying `usize` type, providing atomic operations that guarantee `data consistency` even in `concurrent` environments
         - Thread Safety:   AtomicUsize is thread-safe, while usize is not.
-        - Operations:      AtomicUsize provides atomic operations like fetch_add, compare_and_swap, and load, which are not available for usize.
-        - Memory Ordering: AtomicUsize offers control over memory ordering through Ordering parameters, allowing you to specify the level of synchronization required.
+        - Operations:      AtomicUsize provides atomic operations like `fetch_add`, `compare_and_swap`, and `load`, which are not available for usize.
+        - Memory Ordering: AtomicUsize offers control over memory ordering through Ordering parameters, allowing you to specify the <<level of synchronization required>>.
         - Alignment:       AtomicUsize has stricter alignment requirements than usize to ensure atomic operations work correctly.
 
     > leverages low-level CPU instructions to perform atomic operations. These instructions are hardware-guaranteed to be indivisible, preventing race conditions.
 
 ORDERING:
-    > tells compiler which set of guarantees you expect for this particular memory access wrt things that might be happening in other threads at the same time.
+    > tells compiler which <<set of guarantees>> you expect for this particular <<memory access>> wrt <<things that might be happening in other threads>> at the same time.
 
 >> Atomic provides lots of unique fn to deal with data. Like `store`, `load`, `compare_and_swap`, `swap` etc.
     * When we load and store data separately then there is a chance that a random THREAD can executed data change in between out `load & store` calls.
@@ -19,7 +19,7 @@ MUTEX V/S ATOMIC
     Atomic:
         * Doesn't use locking.
         * Here multiple threads can operate on this value at the same time in some reasonably well-defined way.
-        * All atomic ops are "lock-free" but not necessarily "wait-fre".
+        * All atomic ops are "lock-free" but not necessarily "wait-free".
     Mutex:
         * One thread gets to access the value at a time. And other have to wait until the existing tread releases the lock.
         * Mutex guards larger section of code.
@@ -36,7 +36,7 @@ const UNLOCKED: bool = false;
 /*
 Mutex 2 parameters:
     - Boolean:    marks whether the Lock is UP or not.
-    - UnsafeCell: id we have a lock we can give out Mutable reference of the Inner-type.
+    - UnsafeCell: if we have a lock we can give out << Mutable reference >> of the Inner-type.
 */
 pub struct Mutex<T> {
     locked: AtomicBool,
