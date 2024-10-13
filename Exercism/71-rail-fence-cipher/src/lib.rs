e4fd90225da46fe0f1cc46584d87848a255238f8
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-10-13 22:58:23 +0530

    fix::> rail-fence-cipher init done

---
+++ Exercism/71-rail-fence-cipher/src/lib.rs
@@ -0,0 +1,9 @@
+pub struct RailFence;
+
+impl RailFence {
+    pub fn new(rails: u32) -> RailFence {}
+
+    pub fn encode(&self, text: &str) -> String {}
+
+    pub fn decode(&self, cipher: &str) -> String {}
+}
