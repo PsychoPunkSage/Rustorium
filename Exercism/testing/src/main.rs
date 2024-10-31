aa2534b5d98ea8dcee83dd880accf7f12cdc2ecb
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-10-31 16:17:30 +0530

    fix::> decimal added

--- Exercism/testing/src/main.rs
+++ Exercism/testing/src/main.rs
@@ -1,50 +1,69 @@
-// Trait with an associated type
-trait Converter {
-    type Output;
-
-    fn convert(&self) -> Self::Output;
-}
-
-// Implement the Converter trait for i32 -> String conversion
-impl Converter for i32 {
-    type Output = String;
-
-    fn convert(&self) -> Self::Output {
-        format!("Converted i32: {}", self)
-    }
-}
-
-// Implement the Converter trait for bool -> i32 conversion
-impl Converter for bool {
-    type Output = i32;
-
-    fn convert(&self) -> Self::Output {
-        if *self {
-            1
-        } else {
-            0
-        }
-    }
-}
-
-// Implement the Converter trait for &str -> usize (string length)
-impl Converter for &str {
-    type Output = usize;
-
-    fn convert(&self) -> Self::Output {
-        self.len()
-    }
-}
+// Struct to describe Binary tree... adds integer element in binary tree + search element in binary tree
 
 fn main() {
-    let num = 42;
-    let flag = true;
-    let text = "Hello";
+    let mut tree = Node::new(123);
+    tree.insert(1231);
+    tree.insert(12);
+    println!("{:?}", tree);
+    println!("{}", tree.search(1231));
+    println!("{}", tree.search(1232));
+}
 
-    // Calls convert() on different data types with associated outputs
-    println!("{}", num.convert()); // i32 -> String
-    println!("{}", flag.convert()); // bool -> i32
-    println!("{}", text.convert()); // &str -> usize
-    println!("{}", Converter::convert(&false));
-    println!("{}", Converter::convert(&"false"));
+#[derive(Debug)]
+struct Node {
+    // node left right
+    value: i32,
+    left: Option<Box<Node>>,  // Try to use `Box` somehow
+    right: Option<Box<Node>>, // Try to use `Box` somehow
+}
+
+impl Node {
+    fn new(value: i32) -> Self {
+        Node {
+            value,
+            left: None,
+            right: None,
+        }
+    }
+
+    fn insert(&mut self, value: i32) {
+        /*
+        1. cmp val... value > self.value : right || else left,
+        2. if any val == None place it .... coninut cmp
+         */
+
+        if self.value >= value {
+            match self.left {
+                None => self.left = Some(Box::new(Node::new(value))),
+                Some(ref mut left_child) => left_child.insert(value),
+            }
+        } else {
+            match self.right {
+                None => self.right = Some(Box::new(Node::new(value))),
+                Some(ref mut right_child) => right_child.insert(value),
+            }
+        }
+    }
+
+    fn search(&self, value: i32) -> bool {
+        /*
+        1. self.val == value GOOD
+        2. recursive call search...
+        3. if DNE ??
+         */
+
+        if self.value == value {
+            return true;
+        } else if value < self.value {
+            match self.left {
+                None => false,
+                Some(ref left_child) => left_child.search(value),
+            }
+        } else {
+            match self.right {
+                None => false,
+                Some(ref right_child) => right_child.search(value),
+            }
+        }
+    }
 }
